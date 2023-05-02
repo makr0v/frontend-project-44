ede3973f26b5f185bc2d9169cdacf56e44011509
Vandopal <113090802+makr0v@users.noreply.github.com>
2023-05-02 13:45:38 +0300

    5

--- src/greeting.js
+++ src/greeting.js
@@ -4,5 +4,5 @@ export default function main() {
 console.log(`node bin/brain-games.js`);
 console.log(`Welcome to the Brain Games!`)
 const name = readlineSync.question('May I have your name?: ');
-console.log(`Hello, ${name}`);
+console.log(`Hello, ${name}!`);
 }
