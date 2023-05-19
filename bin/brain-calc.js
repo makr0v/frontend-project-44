c89f952b3dd178626fb063ec47063cee6b72ad16
Vandopal <113090802+makr0v@users.noreply.github.com>
2023-05-19 14:54:57 +0300

    eslint step 4

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-
 export default function Main() {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?: ');
@@ -49,4 +48,4 @@ export default function Main() {
   console.log(`Congratulations, ${name}!`);
 }
 
-Main()
+Main();
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
+// eslint-disable-next-line consistent-return
 export default function even() {
-console.log(`node bin/brain-games.js`);
-console.log(`Welcome to the Brain Games!`)
-const name = readlineSync.question('May I have your name?: ');
-console.log(`Hello, ${name}!`);
-console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
+  console.log('node bin/brain-games.js');
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-for (let i = 0; i <= 4; i++) {
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i <= 4; i++) {
     const randomSign = Math.floor(Math.random() * 100) + 1;
     console.log(`Question: ${randomSign}`);
-    const answer = readlineSync.question("Your answer: ");
+    const answer = readlineSync.question('Your answer: ');
     let yesNo = '';
-    if (answer === 'yes') {yesNo = 'no'}
-    else {yesNo = 'yes'}
-    if (randomSign % 2 === 0 && answer === 'yes') {i += 1, console.log(`Correct!`)}
-    else if (randomSign % 2 !== 0 && answer === 'no') {i += 1, console.log(`Correct!`)}
-    else {return console.log(`Answer "${answer}" if the number is even, otherwise answer "${yesNo}".`)}
+    if (answer === 'yes') { yesNo = 'no'; } else { yesNo = 'yes'; }
+    // eslint-disable-next-line no-unused-expressions, no-sequences
+    if (randomSign % 2 === 0 && answer === 'yes') { i += 1, console.log('Correct!'); } else if (randomSign % 2 !== 0 && answer === 'no') { i += 1, console.log('Correct!'); } else { return console.log(`Answer "${answer}" if the number is even, otherwise answer "${yesNo}".`); }
+  }
+  console.log(`Congratulations, ${name}!`);
 }
-console.log(`Congratulations, ${name}!`)
-}
-even()
+even();
